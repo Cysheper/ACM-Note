dfba6236c0cda08b101fc574349e36f8281d1229
Cysheper <3403699116@qq.com>
2025-04-16 22:37:46 +0800

    none

--- 动态规划dp/线性dp/最长公共子序列.cpp
+++ 动态规划dp/线性dp/最长公共子序列.cpp
@@ -25,14 +25,20 @@ const int mod = 998244353;
 const int N = 1e5 + 5;
 
 void solve() {
-    int n;
-    vector<int> a(n), b(n);
-    for(auto &i: a) cin >> i;
-    for(auto &i: b) cin >> i;
-    vector<vector<int>> dp(n, vector<int>(n));
-    for(int i = 0; i < n; ++ i) {
-        
+    int n; cin >> n;
+    vector<int> a(n + 1), b(n + 1);
+    for(int i = 1; i <= n; ++ i) cin >> a[i];
+    for(int i = 1; i <= n; ++ i) cin >> b[i];
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+    for(int i = 1; i <= n; ++ i) {
+        for(int j = 1; j <= n; ++ j) {
+            if(a[i-1] == b[j-1]) 
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
     }
+    cout << dp[n][n] << "\n";
 }
 
 signed main() {
